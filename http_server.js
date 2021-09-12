3fb8231e6193987cc8f198cbff8e77f032e904be
sahorem <mahesh.sahore@gmail.com>
2021-09-12 09:58:24 -0400

    comment

--- http_server.js
+++ http_server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const app     = express();
-const low     = require('lowdb');
-const fs      = require('lowdb/adapters/FileSync');
+const app = express();
+const low = require('lowdb');
+const fs = require('lowdb/adapters/FileSync');
 const adapter = new fs('db.json');
-const db      = low(adapter);
-const cors    = require('cors');
+const db = low(adapter);
+const cors = require('cors');
 
-// allow cross-origin resource sharing (CORS)
+// allow cross-origin resource sharing (CORS) comment
 app.use(cors());
 
 // data parser - used to parse post data
@@ -19,33 +19,32 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 // init the data store
-db.defaults({ users: []}).write();
+db.defaults({ users: [] }).write();
 
 // return all users
-app.get('/data', function(req, res){     
-    res.send(db.get('users').value());
+app.get('/data', function (req, res) {
+	res.send(db.get('users').value());
 });
 
 // add user
-app.post('/add', function(req, res){
-    var user = {
-        'name'          : req.body.name,
-        'dob'           : req.body.dob,
-        'email'         : req.body.email,
-        'username'      : req.body.username,
-        'password'      : req.body.password,
-        'phone'         : req.body.phone,
-        'streetaddress' : req.body.streetaddress,
-        'citystatezip'  : req.body.citystatezip,
-        'latitude'      : req.body.latitude,
-        'longitude'     : req.body.longitude,
-        'avatar'        : req.body.avatar
-    }
-    db.get('users').push(user).write();
-    // console.log(db.get('users').value());
-    res.send(db.get('users').value());
+app.post('/add', function (req, res) {
+	var user = {
+		name: req.body.name,
+		dob: req.body.dob,
+		email: req.body.email,
+		username: req.body.username,
+		password: req.body.password,
+		phone: req.body.phone,
+		streetaddress: req.body.streetaddress,
+		citystatezip: req.body.citystatezip,
+		latitude: req.body.latitude,
+		longitude: req.body.longitude,
+		avatar: req.body.avatar,
+	};
+	db.get('users').push(user).write();
+	// console.log(db.get('users').value());
+	res.send(db.get('users').value());
 });
-
 
 // --------------------------
 //   for supertest testing
@@ -54,9 +53,9 @@ app.post('/add', function(req, res){
 
 // start server
 // -----------------------
-app.listen(3000, function(){
-    console.log('Running on port 3000!')
-})
+app.listen(3000, function () {
+	console.log('Running on port 3000!');
+});
 
 // export app for testing
 module.exports = app;
